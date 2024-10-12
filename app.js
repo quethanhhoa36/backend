889e92e35438717c7034c97f1a76ceb1aa8d5c72
quethanhhoa36 <cngo98279@gmail.com>
2024-10-12 08:34:02 +0700

    Cai dat them user

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const express = require("express");
 const app = express();
 const cors = require('cors');
+const ApiError = require("./app/api-error");
+const userRouter=require("./app/routes/user.route");
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/', (req,res)=>{
-    res.json({message: "welcome to bookstore"});
-})
+app.use("/api/user",userRouter);
+
+app.get('/', (req, res) => {
+    res.json({ message: "Welcome to contact book aplication." });
+});
+app.use((req,res,next) =>{
+    return next(new ApiError(404,"Resource not found"));
+});
+
+app.use((error,req,res,next)=>{
+    return res.status(error.statusCode || 500).json({
+        message: error.message || "Internal Server Error",
+    });
+});
+
 
 module.exports = app;
